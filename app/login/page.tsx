8d1a341e16a2e9a1ce42cc641d591c4714742080
talhasohail56 <talhasohail56@gmail.com>
2025-12-12 08:00:51 +0500

    fix: add missing Image import to login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import { useState } from 'react'
+import Image from 'next/image'
 import { signIn } from 'next-auth/react'
 import { useRouter } from 'next/navigation'
 import { Button } from '@/components/ui/button'
